0cd5ef40dc70875be50fbb6f5d761fbab10f5cd6
Chacha Chacha <calex2607@gmail.com>
2025-07-05 16:18:39 +0300

    safe exports

---
+++ photo_booth/src/hooks/index.ts
@@ -0,0 +1,8 @@
+// src/hooks/index.ts
+export * from './useCamera';
+export * from './usePhotoCapture';
+export * from './useCanvas';
+export * from './useLocalStorage';
+export * from './useErrorHandler';
+export * from './usePermissions';
+export * from './useDebounce';
